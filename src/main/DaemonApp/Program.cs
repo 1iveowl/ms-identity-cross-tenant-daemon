41f8b4c5fb16627ac665881784e57836ccf05562
Jasper Hedegaard Bojsen <jasperhb@outlook.com>
2021-11-05 13:43:04 +0100

    Start

--- src/main/DaemonApp/Program.cs
+++ src/main/DaemonApp/Program.cs
@@ -1,42 +1,78 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Azure.Identity;
+using DaemonApp.Options;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Graph;
 using Microsoft.Identity.Client;
 using Options;
 
+// .NET 6 - See https://aka.ms/new-console-template for more information
+
+// Read configuration settings.
 var builder = new ConfigurationBuilder()
-    .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.Development.json", optional: false);
+    .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.Development.json", optional: false)
+    .AddJsonFile("daemonAppSettings.json", optional: false);
 
 var configuration = builder.Build();
 
-var tenantId = configuration
-    .GetSection(AzureAdOptions.AzureAdAppRegistration)
-    .GetSection(nameof(AzureAdOptions.TenantId))
-    .Value;
+var deamonAppSettings = configuration
+    .GetSection(DaemonAppOptions.DaemonAppSettings);
 
-var clientId = configuration
-    .GetSection(AzureAdOptions.AzureAdAppRegistration)
-    .GetSection(nameof(AzureAdOptions.ClientId))
-    .Value;
 
-var secret = configuration
-    .GetSection(AzureAdOptions.AzureAdAppRegistration)
-    .GetSection(nameof(AzureAdOptions.ClientSecret))
-    .Value;
+var azureAppRegistration = configuration
+    .GetSection(AzureAdOptions.AzureAdAppRegistration);
 
-var authority = configuration
-    .GetSection(AzureAdOptions.AzureAdAppRegistration)
-    .GetSection(nameof(AzureAdOptions.Authority))
-    .Value;
+var tenantId = deamonAppSettings.GetSection(nameof(DaemonAppOptions.TenantId)).Value;
+var clientId = azureAppRegistration.GetSection(nameof(AzureAdOptions.ClientId)).Value;
+var secret = azureAppRegistration.GetSection(nameof(AzureAdOptions.ClientSecret)).Value;
 
-var app = ConfidentialClientApplicationBuilder.Create(clientId)
-    .WithClientSecret(secret)
-    .WithAuthority(new Uri($"https://login.microsoftonline.com/{tenantId}"))
-    .WithTenantId(tenantId)
-    .Build();
 
-var scopes = new string[] { $"https://graph.microsoft.com/.default", };
+// #1 let's first get the access token so that we can examine it.
+var accessToken = await GetAccessToken();
+Console.WriteLine($"Access Token (Decode token using https://jwt.ms)\r\n{accessToken}\r\n");
 
-var result = await app.AcquireTokenForClient(scopes).ExecuteAsync();
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// #2 Let's use Microsoft Graph to get data.
+Console.WriteLine($"List all users in tenant (requires the 'User.Read.All' Application Permission):");
+await WriteAllUserNamesInTenantFromMSGraph();
+
+Console.ReadLine();
+
+
+// https://docs.microsoft.com/en-us/graph/sdks/choose-authentication-providers?tabs=CS#using-a-client-secret
+async Task WriteAllUserNamesInTenantFromMSGraph()
+{
+    
+    var options = new TokenCredentialOptions
+    {
+        AuthorityHost = AzureAuthorityHosts.AzurePublicCloud
+    };
+        
+    var clientSecretCredentials = new ClientSecretCredential(tenantId, clientId, secret, options);
+
+    var graphClient = new GraphServiceClient(clientSecretCredentials);
+
+    var users = await graphClient.Users.Request().GetAsync();
+
+
+    foreach (var user in users)
+    {
+        Console.WriteLine(user.DisplayName);
+    }
+}
+
+async Task<string> GetAccessToken()
+{
+    // https://docs.microsoft.com/en-us/graph/auth-v2-service#4-get-an-access-token
+    var scopes = new string[] { $"https://graph.microsoft.com/.default", };
+
+    var app = ConfidentialClientApplicationBuilder.Create(clientId)
+        .WithClientSecret(secret)
+        .WithAuthority($"https://login.microsoftonline.com/{tenantId}")
+        .WithTenantId(tenantId)
+        .Build();
+
+    var authenticationResult = await app.AcquireTokenForClient(scopes).ExecuteAsync();
+
+    return authenticationResult.AccessToken;
+}
