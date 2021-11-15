b018656c2017a4ecbf45a76eeb85257e2ba2e351
Jasper Hedegaard Bojsen <jasperhb@outlook.com>
2021-11-15 11:57:25 +0100

    Check for config file

--- src/main/landingPage/Program.cs
+++ src/main/landingPage/Program.cs
@@ -4,44 +4,7 @@ using Microsoft.Identity.Web;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
 
-var builder = WebApplication.CreateBuilder(args);
-var configuration = builder.Configuration;
-
-// Using the Options for settings: https://docs.microsoft.com/en-us/aspnet/core/fundamentals/configuration/options?view=aspnetcore-6.0
-builder.Services.Configure<AzureAdOptions>(configuration.GetSection(AzureAdOptions.AzureAdAppRegistration));
-builder.Services.Configure<MSGraphOptions>(configuration.GetSection(MSGraphOptions.MSGraphSettings));
-builder.Services.Configure<DaemonPermissionOptions>(configuration.GetSection(DaemonPermissionOptions.DaemonPermissions));
-
-string[]? initialScopes = configuration.GetValue<string>($"{MSGraphOptions.MSGraphSettings}:Scopes")?.Split(' ');
-
-
-// https://github.com/Azure-Samples/active-directory-aspnetcore-webapp-openidconnect-v2/tree/master/2-WebApp-graph-user/2-3-Multi-Tenant
-builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-                .AddMicrosoftIdentityWebApp(configuration.GetSection(AzureAdOptions.AzureAdAppRegistration))
-                .EnableTokenAcquisitionToCallDownstreamApi(initialScopes)
-                .AddMicrosoftGraph(configuration.GetSection(MSGraphOptions.MSGraphSettings))
-                .AddInMemoryTokenCaches();
-
-builder.Services.AddControllersWithViews(options =>
-{
-    var policy = new AuthorizationPolicyBuilder()
-        .RequireAuthenticatedUser()
-        .Build();
-    options.Filters.Add(new AuthorizeFilter(policy));
-});
-
-builder.Services.AddAuthorization(options =>
-{
-    // By default, all incoming requests will be authorized according to the default policy
-    options.FallbackPolicy = options.DefaultPolicy;
-
-});
-
-// Add services to the container.
-builder.Services.AddRazorPages();
-
-
-var app = builder.Build();
+var app = BuildApp();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -62,3 +25,51 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
+
+
+WebApplication BuildApp()
+{
+    const string settingsFile = "appsettings.Development.json";
+
+    var builder = WebApplication.CreateBuilder(args);
+    var configuration = builder.Configuration;
+
+    if (!File.Exists(settingsFile))
+    {
+        throw new Exception($"Please create and fill the file { settingsFile }. See the readme for details.");
+    }
+
+    // Using the Options for settings: https://docs.microsoft.com/en-us/aspnet/core/fundamentals/configuration/options?view=aspnetcore-6.0
+    builder.Services.Configure<AzureAdOptions>(configuration.GetSection(AzureAdOptions.AzureAdAppRegistration));
+    builder.Services.Configure<MSGraphOptions>(configuration.GetSection(MSGraphOptions.MSGraphSettings));
+    builder.Services.Configure<DaemonPermissionOptions>(configuration.GetSection(DaemonPermissionOptions.DaemonPermissions));
+
+    string[]? initialScopes = configuration.GetValue<string>($"{MSGraphOptions.MSGraphSettings}:Scopes")?.Split(' ');
+
+    // https://github.com/Azure-Samples/active-directory-aspnetcore-webapp-openidconnect-v2/tree/master/2-WebApp-graph-user/2-3-Multi-Tenant
+    builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+                    .AddMicrosoftIdentityWebApp(configuration.GetSection(AzureAdOptions.AzureAdAppRegistration))
+                    .EnableTokenAcquisitionToCallDownstreamApi(initialScopes)
+                    .AddMicrosoftGraph(configuration.GetSection(MSGraphOptions.MSGraphSettings))
+                    .AddInMemoryTokenCaches();
+
+    builder.Services.AddControllersWithViews(options =>
+    {
+        var policy = new AuthorizationPolicyBuilder()
+            .RequireAuthenticatedUser()
+            .Build();
+        options.Filters.Add(new AuthorizeFilter(policy));
+    });
+
+    builder.Services.AddAuthorization(options =>
+    {
+        // By default, all incoming requests will be authorized according to the default policy
+        options.FallbackPolicy = options.DefaultPolicy;
+
+    });
+
+    // Add services to the container.
+    builder.Services.AddRazorPages();
+
+    return builder.Build();
+}
